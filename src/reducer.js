ba213084ff4a2894121b155c48d7618472da35a6
Debolu <obayemiadebolu16@gmail.com>
2023-02-21 21:17:43 +0100

    Cart component created with React.Js

---
+++ src/reducer.js
@@ -0,0 +1,56 @@
+import CartItem from "./CartItem";
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE") {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+    };
+  }
+  if (action.type === "INCREASE") {
+    const tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "DECREASE") {
+    const tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "GET_TOTALS") {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem;
+        cartTotal.amount += amount;
+
+        const itemTotal = amount * price;
+        cartTotal.total += itemTotal;
+
+        return cartTotal;
+      },
+      {
+        total: 0,
+        amount: 0,
+      }
+    );
+
+    total = parseFloat(total.toFixed(2));
+
+    return { ...state, total, amount };
+  }
+  return state;
+};
+
+export default reducer;
